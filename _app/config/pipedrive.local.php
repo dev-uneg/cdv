7b1511b286b7d52a70e9c15f241349d1fc8a8ef1
dev-uneg <developer@uneg.edu.mx>
2026-03-03 16:22:22 -0600

    _fix pipedrive

---
+++ _app/config/pipedrive.local.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'api_token' => '7a9fb31f5f176862c8d612ac8f54d86350394a23',
+    'custom_fields' => [
+        'interest' => 'cd1724715699c7674b53fd7e5918a1c853fa340f',
+        'medium' => '1cd81947451e14a3c30084a31db4d6eef6fef63e',
+    ],
+];
